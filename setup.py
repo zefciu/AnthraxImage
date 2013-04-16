216e615b0120ea2c3249cb26d478ed20f1136dfa
Szymon Pyżalski <szymon@pythonista.net>
2013-04-16 07:27:58 +0200

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name = 'AnthraxImage',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Szymon Pyżalski',
     author_email = 'zefciu <szymon@pythonista.net>',
     description = 'Anthrax - tools for HTML input',
